c4de2dac6bf43854fde069427f42f668f25a89fc
sulfikaralijun <sulfikaralijun@gmail.com>
2024-09-18 10:54:33 +0800

    init

---
+++ app/src/main/java/com/sulfikar/composearticle/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.sulfikar.composearticle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sulfikar.composearticle.ui.theme.ComposeArticleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeArticleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeArticle()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeArticle(){
+    Column {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null
+        )
+        Text(
+            text = "Jetpack Compose tutorial",
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.paragraph_text_1),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(R.string.paragraph_text_2),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    ComposeArticleTheme {
+        ComposeArticle()
+    }
+}
